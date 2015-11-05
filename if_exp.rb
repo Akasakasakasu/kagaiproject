35bb470f1caffdef406c886285e3a5fecc4eda9b
s123043 <kougamishinya645@gmail.com>
2015-11-05 16:47:14 +0900

    簡略化をした

--- if_exp.rb
+++ if_exp.rb
@@ -1,9 +1,9 @@
 a, b = STDIN.gets.split.map(&:to_i)
 
-if a < b then
-  puts'a < b'
-elsif a > b then
-  puts'a > b'
-else 
-  puts'a == b'
-end
+puts (if a < b then
+        'a < b'
+      elsif a > b then
+        'a > b'
+      else 
+        'a == b'
+      end)
