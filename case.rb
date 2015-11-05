a8cc271972f2022cb3d3c28b01b8d0d119c3b78e
s123043 <kougamishinya645@gmail.com>
2015-11-05 17:10:00 +0900

    簡略化した

--- case.rb
+++ case.rb
@@ -4,22 +4,19 @@ loop{ #無限ループをするメソッド
   a, op, b = STDIN.gets.split
   a = a .to_i
   b = b .to_i
-  c = c .to_i
   op = op .to_s
-  case op
-  when '+' 
-    c = a + b
-    puts c
-  when '-' 
-    c = a - b
-    puts c
-  when '*' 
-    c = a * b
-    puts c
-  when '/'  
-    c = a/b
-    puts c
-  when '?'
-    break
-  end
-}
+  puts( case op
+        when '+' 
+          a + b
+        when '-' 
+          a - b
+        when '*' 
+          a * b
+        when '/'  
+          c = a/b
+        when '?'
+          break
+        end)
+      }
+      
+      
