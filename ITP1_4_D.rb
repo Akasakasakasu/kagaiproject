c56865f9ab9a7f85557dfb29152847127441e530
s123043 <kougamishinya645@gmail.com>
2015-11-18 16:41:41 +0900

    最大、最小、合計を求めるプログラムを作成した

---
+++ ITP1_4_D.rb
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+STDIN.gets
+arr = STDIN.gets.split.map(&:to_i)
+
+min = arr.inject(1000000) do |cmin, i|
+  if cmin < i then
+    cmin
+  else 
+    i
+  end
+end
+
+max = arr.inject(-1000000) do |cmax, i|
+  if cmax > i then
+    cmax
+  else
+    i 
+  end
+end
+
+sum = arr.inject(&:+)
+
+puts "#{min} #{max} #{sum}"
